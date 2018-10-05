a73bfa8df5a9668927dc7a31d666519365152404
harmeet651 <harmeet651@gmail.com>
2018-10-05 11:28:58 -0700

    updated code

--- src/codingNinjas/QuickSort.java
+++ src/codingNinjas/QuickSort.java
@@ -14,10 +14,44 @@ public class QuickSort {
 			//int arr[]= {12,1,3,4};
 			int l=arr.length;
 			QuickSort ob = new QuickSort();
-			ob.QuickSort2(arr, 0, l-1);		 
+			ob.quickSort(arr, 0, l-1);		 
 	        System.out.println("sorted array");
 	        printArray(arr);
 	}
+	static int[] quickSort(int[] input,int s,int e)
+	{
+		if(s>=e)
+		{
+			int t[]={};
+			return t;
+		}
+		int pivotindex=getpivotelement(input,s,e);
+		quickSort(input,s,pivotindex-1);
+		quickSort(input,pivotindex+1,e);
+		return input;
+	}
+
+static int getpivotelement(int []input,int s,int e)
+{	
+	int pi=s;
+	int pe=input[e];
+	for(int i=s;i<e;i++)
+	{
+		if(input[i]<=pe)
+		{
+			int t=0;
+			t=input[i];
+			input[i]=input[pi];
+			input[pi]=t;
+			pi++;
+		}
+	}
+	int a=input[pi];
+	input[pi]=input[e];
+	input[e]=a;
+	e=a;
+	return pi;
+}
 	static void printArray(int arr[])
     {
         int n = arr.length;
@@ -25,36 +59,36 @@ public class QuickSort {
             System.out.print(arr[i]+" ");
         System.out.println();
     }
-	public static void QuickSort2(int[] arr, int start, int end)
-	{
-		if(start<end)
-		{
-			int pi = partition(arr, start, end);
-			QuickSort2(arr, start, pi-1);
-			QuickSort2(arr, pi+1, end);
-		}		
-	}
-	public static int partition(int[] arr, int start, int end)
-	{
-		int pivot = arr[end];
-		int low=start-1;
-		int j=start;
-		while(j<end)
-		{
-			if(arr[j]<=pivot)
-			{
-				low++;
-				int temp=arr[j];
-				arr[j]= arr[low];
-				arr[low]=temp;
-			}
-		}
-	
-			int temp=arr[low+1];
-			arr[low+1]=arr[end];
-			arr[end]=temp;
-
-		return low+1;
-
-	}
+//	public static void QuickSort2(int[] arr, int start, int end)
+//	{
+//		if(start<end)
+//		{
+//			int pi = partition(arr, start, end);
+//			QuickSort2(arr, start, pi-1);
+//			QuickSort2(arr, pi+1, end);
+//		}		
+//	}
+//	public static int partition(int[] arr, int start, int end)
+//	{
+//		int pivot = arr[end];
+//		int low=start-1;
+//		int j=start;
+//		while(j<end)
+//		{
+//			if(arr[j]<=pivot)
+//			{
+//				low++;
+//				int temp=arr[j];
+//				arr[j]= arr[low];
+//				arr[low]=temp;
+//			}
+//		}
+//	
+//			int temp=arr[low+1];
+//			arr[low+1]=arr[end];
+//			arr[end]=temp;
+//
+//		return low+1;
+//
+//	}
 }
