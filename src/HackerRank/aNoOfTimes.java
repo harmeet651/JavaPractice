f2da28ef54d78acd02851369c9d2a783aa73e2c5
harmeet651 <harmeet651@gmail.com>
2018-10-26 10:52:46 -0700

    practice

---
+++ src/HackerRank/aNoOfTimes.java
@@ -0,0 +1,36 @@
+package HackerRank;
+
+public class aNoOfTimes {
+
+	public static void main(String[] args) {
+		int n=10;
+		String s ="aba";
+		int func = aNo(n,s);
+		System.out.println(func);
+	}
+	public static int aNo(int n, String s)
+	{
+		 int withinNchar = n/s.length();
+		 int remaining = n%s.length();
+		 int aInS=0;
+		 int c=0;
+		 for(int i=0;i<s.length();i++)
+		 {
+			 int a = s.charAt(i);
+			 if(a=='a')
+			 {
+				 aInS++;
+			 }
+		 }
+		 for(int i=0;i<remaining;i++)
+		 {
+			 int a = s.charAt(i);
+			 if(a=='a')
+			 {
+				 c++;
+			 }
+		 }
+		 int totalWithoutRem= aInS*withinNchar+c;
+		 return totalWithoutRem;
+	}
+}
