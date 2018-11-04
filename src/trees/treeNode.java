3a3cd3d01f0810398a503c5df34bb4a683ada545
harmeet651 <harmeet651@gmail.com>
2018-11-03 22:30:09 -0700

    Tree practice

---
+++ src/trees/treeNode.java
@@ -0,0 +1,13 @@
+package trees;
+
+import java.util.ArrayList;
+
+public class treeNode<T> {
+	public T data;
+	public ArrayList<treeNode<T>> children;
+	
+	public treeNode(T data) {
+		this.data = data;
+		children = new ArrayList<>(); 
+	}
+}
