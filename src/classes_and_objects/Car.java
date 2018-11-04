3fc4729df4ab150b9de98371536f7e1635bcb648
harmeet651 <harmeet651@gmail.com>
2018-11-04 01:00:56 -0800

    @Override added

--- src/classes_and_objects/Car.java
+++ src/classes_and_objects/Car.java
@@ -14,6 +14,10 @@ public class Car extends Vehicle {
 	}
 
 	@Override
+	//override -Use it every time you override a method for two benefits. Do it so that you can take advantage of the compiler checking
+	//to make sure you actually are overriding a method when you think you are. This way, if you make a common mistake of misspelling a
+	//method name or not correctly matching the parameters, you will be warned that your method does not actually override as you think 
+	//it does. Secondly, it makes your code easier to understand because it is more obvious when methods are overwritten.
 	public boolean isMotorized() 
 	{	
 		return false;
