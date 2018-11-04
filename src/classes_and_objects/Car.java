238b186dd1ba40bd5f0a84eefa38be1832bf2e14
harmeet651 <harmeet651@gmail.com>
2018-11-04 01:57:25 -0700

    abstract class, final concept added

--- src/classes_and_objects/Car.java
+++ src/classes_and_objects/Car.java
@@ -5,12 +5,18 @@ public class Car extends Vehicle {
 	public int noOfGears;
 	public int modelYear;
 	
-	public void print()
+	public void printCar()
 	{
 		System.out.println("Car color is : "+color);
 		System.out.println("Car speed is : "+getMaxSpeed());
 		System.out.println("Car has these many gears : "+noOfGears);
 		System.out.println("Car model is : "+modelYear);
+	}
+
+	@Override
+	public boolean isMotorized() 
+	{	
+		return false;
 	}
 	
 }
--- src/classes_and_objects/Vehicle.java
+++ src/classes_and_objects/Vehicle.java
@@ -1,6 +1,6 @@
 package classes_and_objects;
 
-public class Vehicle {
+public abstract class Vehicle {
 	public String color;
 	private int maxSpeed;
 	
@@ -14,10 +14,14 @@ public class Vehicle {
 		this.maxSpeed = maxSpeed;
 	}
 	
-	public void print()
+	//final print can't name a print in car and call it now
+	public final void print()
 	{
 		System.out.println("Vehicle color is : "+color);
 		System.out.println("Vehicle speed is : "+maxSpeed);
 	}
+	
+	//abstract method, completed in car, due to this we have made the class vehicle abstract, now cannit make object of vehicle
+	public abstract boolean isMotorized();
 
 }
