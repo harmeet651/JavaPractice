c217d40ad98bab80f6ef8a1b23a5a7788ab2b97e
harmeet651 <harmeet651@gmail.com>
2018-12-18 07:25:33 -0800

    practice

--- src/LeetCode/ContainsDuplicate2.java
+++ src/LeetCode/ContainsDuplicate2.java
@@ -2,8 +2,9 @@ package LeetCode;
 
 import java.util.HashMap;
 import java.util.HashSet;
-
-public class containsDuplicate {
+//Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such 
+//that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+public class ContainsDuplicate2 {
 
 	public static void main(String[] args) {
 		int arr[] = {1,2,3,1};
